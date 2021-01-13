aa9475d43c653f0b62f83c79e366bcb4b80b238a
loringmoore <lobo.moore@gmail.com>
2021-01-12 19:47:35 -0500

    map and spells
    minor map changes, added knight element spells

---
+++ data/spells/scripts/attack/water sweep.lua
@@ -0,0 +1,17 @@
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_DROWNDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_LOSEENERGY)
+combat:setParameter(COMBAT_PARAM_BLOCKARMOR, 1)
+combat:setArea(createCombatArea(AREA_WAVE6, AREADIAGONAL_WAVE6))
+
+function onGetFormulaValues(player, skill, attack, factor)
+	local skillTotal = skill * attack
+	local levelTotal = player:getLevel() / 5
+	return -(((skillTotal * 0.04) + 31) + (levelTotal)), -(((skillTotal * 0.08) + 45) + (levelTotal))
+end
+
+combat:setCallback(CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, var)
+	return combat:execute(creature, var)
+end
