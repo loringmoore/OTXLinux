8a4694f5ec9de01e50e085d179439780e94e4a87
loringmoore <lobo.moore@gmail.com>
2021-01-15 17:01:03 -0500

    Update firstitems.lua

--- data/creaturescripts/scripts/firstitems.lua
+++ data/creaturescripts/scripts/firstitems.lua
@@ -16,7 +16,7 @@ local config = {
 		--equipment: wooden shield, jacket, leather legs, leather helmet
 		items = {{2512, 1}, {2650, 1}, {2649, 1}, {2461, 1}},
 		--container rope, shovel, hand axe, rapier, wand of vortex, spellbook
-		container = {{2120, 1}, {2554, 1}, {2389, 5}, {2380, 1}, {2384, 1}, {2401, 1}, {2190,1}, {2175,1}}
+		container = {{2175,1}}
 	},
 	[4] = {
 		--equipment dwarven shield, steel axe, brass armor, brass helmet, brass legs scarf
