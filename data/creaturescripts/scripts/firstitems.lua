74b963cbd6ac7733cff188af8a62b88ac9908429
loringmoore <lobo.moore@gmail.com>
2021-01-15 16:53:44 -0500

    Update firstitems.lua

--- data/creaturescripts/scripts/firstitems.lua
+++ data/creaturescripts/scripts/firstitems.lua
@@ -14,7 +14,7 @@ local config = {
 	},
 	[3] = {
 		--equipment: wooden shield, jacket, leather legs, leather helmet
-		items = {{2512, 1}, { {2650, 1}, {2649, 1}, {2461, 1}},
+		items = {{2512, 1}, {2650, 1}, {2649, 1}, {2461, 1}},
 		--container rope, shovel, hand axe, rapier, wand of vortex, spellbook
 		container = {{2120, 1}, {2554, 1}, {2389, 5}, {2380, 1}, {2384, 1}, {2401, 1}, {2190,1}, {2175,1}}
 	},
@@ -23,8 +23,8 @@ local config = {
 		items = {{2525, 1}, {2465, 1}, {2460, 1}, {2478, 1}, {2643, 1}, {2661, 1}},
 		--container jagged sword, daramian mace, rope, shovel, health potion
 		container = {{2439, 1}, {2120, 1}, {2554, 1}}
+		}
 	}
-}
 
 function onLogin(player)
 	local targetVocation = config[player:getVocation():getId()]
