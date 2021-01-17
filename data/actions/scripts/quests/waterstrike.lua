ed4555f0a05480753422d310395a874db379604c
loringmoore <lobo.moore@gmail.com>
2021-01-17 17:18:21 -0500

    Update waterstrike.lua

--- data/actions/scripts/quests/waterstrike.lua
+++ data/actions/scripts/quests/waterstrike.lua
@@ -4,7 +4,9 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	else if target.itemid == 5501 and target.uid == 3000 and player:getStorageValue(6032) == 0 then
 		player:setStorageValue(6032, 1)
 		player:addItem(5436, 1)
+		player:sendTextMessage(MESSAGE_INFO_DESCR, 'You have found a scroll.')
 		toPosition:sendMagicEffect(CONST_ME_MAGIC_BLUE)
 	end
 	return true
+	end
 end
