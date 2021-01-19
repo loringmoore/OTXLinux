2b47b4e48b9854310d5cb01413b63b9ad0ad2e42
loringmoore <lobo.moore@gmail.com>
2021-01-19 08:58:27 -0500

    Update desertquestkey.lua

--- data/actions/scripts/quests/desertquestkey.lua
+++ data/actions/scripts/quests/desertquestkey.lua
@@ -1,9 +1,12 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	
+	local item = player:addItem(2089)
+	
 	if player:getStorageValue(6020) == 1 then
 		player:sendTextMessage(MESSAGE_INFO_DESCR, 'The mushroom is empty.')
 	else
 		player:sendTextMessage(MESSAGE_INFO_DESCR, 'You have found a copper key.')
-		player:addItem(2089, 1)
+		player:addItem(2089)
 		item:setActionId(5821)
 		player:setStorageValue(6020, 1)
 	end
