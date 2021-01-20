cc2be7e176fb2121f9b1066314742b53484e9c72
loringmoore <lobo.moore@gmail.com>
2021-01-19 21:28:25 -0500

    Update bugreport.lua

--- data/talkactions/scripts/bugreport.lua
+++ data/talkactions/scripts/bugreport.lua
@@ -1,7 +1,6 @@
-
 function onSay(cid, words, param, channel)
 local storage = 6707
-local exhaust = 10
+local delaytime = 600
 local a = "data/logs/bugs.txt"
 local f = io.open(a, "a+")
 	if(param == '') then
@@ -12,10 +11,10 @@ local exhaust = exhaustion.get(cid, storage)
 	if(not exhaust) then
 		exhaustion.set(cid, storage, delaytime)
                 doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "Your report has been received successfully!")
-		f:write(""..getPlayerName(cid).." reported a bug at " .. os.date("%d %B %Y - %X.", os.time()) .."\n"..param.." [x="..getPlayerPosition(cid).x..", y="..getPlayerPosition(cid).y..", z="..getPlayerPosition(cid).z.."].\n\n----------------------------------------------------------\n")
+		f:write(""..getPlayerName(cid).." reported a bug at " .. os.date("%d %B %Y - %X ", os.time()) .."\n"..param.." [x="..getPlayerPosition(cid).x..", y="..getPlayerPosition(cid).y..", z="..getPlayerPosition(cid).z.."].\n\n----------------------------------------------------------\n")
 		f:close()
 	else
-		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_RED, "You must wait " .. exhaustion.get(cid, storage) .. " seconds to report.")
+		doPlayerSendCancel(cid, "You are exhausted.")
 	end
 return TRUE
 end
