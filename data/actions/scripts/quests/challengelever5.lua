923a427a2d01b8f07d5be1eb72a0d11b5c615f39
loringmoore <lobo.moore@gmail.com>
2021-02-10 19:40:34 -0500

    7

--- data/actions/scripts/quests/challengelever5.lua
+++ data/actions/scripts/quests/challengelever5.lua
@@ -6,14 +6,14 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	if item.itemid == 1945 then
 		local tile = t[1]:getTile()
 		if tile then
-			local stone = tile:getItemById(1544)
+			local stone = tile:getItemById(3514)
 			if stone then
 				stone:remove()
 			end
 		end
 		
 	elseif item.itemid == 1946 then
-		Game.createItem(1544, 1, t[1])
+		Game.createItem(3514, 1, t[1])
 	end
 	return item:transform(item.itemid == 1945 and 1946 or 1945)
 end
