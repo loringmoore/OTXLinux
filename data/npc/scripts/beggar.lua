2d2a597f0d3383fbcbb3cd1f3b6a3bded21c3c68
loringmoore <lobo.moore@gmail.com>
2021-03-23 18:32:58 -0400

    Update beggar.lua

--- data/npc/scripts/beggar.lua
+++ data/npc/scripts/beggar.lua
@@ -22,27 +22,32 @@ local function creatureSayCallback(cid, type, msg)
 			npcHandler:say("I guess I must be out of touch with the world of fashion of this is what's 'in' right now. Very well, if you are willing to collect some things for me, I can give you an outfit just like mine. What do you say?", cid)
 			npcHandler.topic[cid] = 2
 		end
-	elseif msgcontains(msg, "initiation") then
+	elseif msgcontains(msg, "staff") then
 		if player:getStorageValue(6080) == 1 then
-			npcHandler:say("Did you collect 50 hardened bones, 5 bast skirts and a hydra egg?", cid)
+			npcHandler:say("Did you bring me a new staff?", cid)
 			npcHandler.topic[cid] = 4
 		end
-	elseif msgcontains(msg, "staff") then
+	elseif msgcontains(msg, "beard") then
 		if player:getStorageValue(6080) == 2 then
-			npcHandler:say("Did you bring back 2 voodoo dolls and a banana staff?", cid)
+			npcHandler:say("Did you bring me a new beard?", cid)
 			npcHandler.topic[cid] = 5
 		end
-	elseif msgcontains(msg, "mask") then
+	elseif msgcontains(msg, "necklace") then
 		if player:getStorageValue(6080) == 3 then
-			npcHandler:say("Did you find 3 tribal masks and 10 enchanted chicken wings?", cid)
+			npcHandler:say("Did you find my necklace?", cid)
 			npcHandler.topic[cid] = 6
+		end
+	elseif msgcontains(msg, "cloth") then
+		if player:getStorageValue(6080) == 3 then
+			npcHandler:say("Did you collect 100 brown pieces of cloth?", cid)
+			npcHandler.topic[cid] = 7
 		end
 	elseif msgcontains(msg, "yes") then
 		if npcHandler.topic[cid] == 2 then
 			npcHandler:say({
 				"Alright if that's what you really want. ...",
-				"First, you'll need a staff. Something sturdy, like an amber staff. ...",
-				"You don't look like you're capable of growing a beard like mine, so you'll have to find one. Bring me a dwarven beard and I'll see what I can do. ...",
+				"First, you'll need a staff. Something sturdy, like an amber staff. If you get me a new one, I'll give you mine. ...",
+				"I'll let you in on a secret - this isn't my real beard, it's actually a dwarven beard! It's starting to get pretty gross though, I need you to bring me a new one. ...",
 				"Next, I need you to do me a favor. I once lost a valuable necklace deep in spider's cave under the mountain. I'm too lazy to retrieve it, so I'll ask that you do it for me. ...",
 				"Finally, I'll need some material to work with. Bring me...say...100 brown pieces of cloth. ...",
 				"You still want to work with me, |PLAYERNAME|?"
@@ -53,35 +58,42 @@ local function creatureSayCallback(cid, type, msg)
 			player:setStorageValue(6080, 1)
 			npcHandler.topic[cid] = 0
 		elseif npcHandler.topic[cid] == 4 then
-			if player:removeItem(5289, 50) and player:removeItem(3963, 5) and player:removeItem(4850, 1) then
-				npcHandler:say("Well done |PLAYERNAME| - let the ritual commence! <Chants incoherently> Now, bring the materials to fashion a staff!", cid)
+			if player:removeItem(5212, 1) then
+				npcHandler:say("Ha! Thanks for bringing me a new staff, you can have my old one. Now go get that new beard I asked for!", cid)
 				player:setStorageValue(6080, 2)
 				npcHandler.topic[cid] = 0
 			else
-				npcHandler:say("You did not bring what I asked!", cid)
+				npcHandler:say("Do you want me to help you or not?", cid)
 			end
 		elseif npcHandler.topic[cid] == 5 then
-			if player:removeItem(3955, 2) and player:removeItem(3966, 1) then
-				npcHandler:say("Aaahh, yes, yes! These dolls are full of voodoo magic and this staff is in pristine condition! Bring me the final pieces for your mask and your journey will be complete!", cid)
+			if player:removeItem(5270, 1) then
+				npcHandler:say("Wow, it's perfect! Now go find that necklace I lost.", cid)
 				player:setStorageValue(6080, 3)
 				npcHandler.topic[cid] = 0
 			else
-				npcHandler:say("You did not bring what I asked!", cid)
+				npcHandler:say("Do you want me to help you or not?", cid)
 			end
 		elseif npcHandler.topic[cid] == 6 then
-			if player:getItemCount(3967) >= 3 and player:getItemCount(5031) >= 10 then
-				npcHandler:say("Well done, |PLAYERNAME|! You have proven yourself ready and worthy to walk the Shamanic path. May the spirits guide you!", cid)
-				player:removeItem(3967, 3)
-				player:removeItem(5031, 10)
+			if player:removeItem(2132, 1) then
+				npcHandler:say("To be honest, I didn't actually think you'd survive. You know what, you can keep the necklace. Wear it if you want, I don't care.", cid)
 				player:setStorageValue(6080, 4)
+				npcHandler.topic[cid] = 0
+			else
+				npcHandler:say("Do you want me to help you or not?", cid)
+			end
+		elseif npcHandler.topic[cid] == 7 then
+			if player:getItemCount(5281) >= 100 then
+				npcHandler:say("Well, I suppose you've done everything I asked. Enjoy your outfit...I guess.", cid)
+				player:removeItem(5281, 100)
+				player:setStorageValue(6080, 5)
 				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
 				npcHandler.topic[cid] = 0
 			else
-				npcHandler:say("You did not bring what I asked!", cid)
+				npcHandler:say("Do you want me to help you or not?", cid)
 			end
 	elseif msgcontains(msg, "no") then
 		if npcHandler.topic[cid] > 1 then
-			npcHandler:say("Then no.", cid)
+			npcHandler:say("Whatever, and people say I'M weird.", cid)
 			npcHandler.topic[cid] = 0
 		end
 	end
